e07126daea3447e0ecd641882d40d13bb870d3ff
Kamna chauhan <kamnac78@gmail.com>
2025-12-26 13:01:44 +0530

    connect to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,29 @@
+ import express from "express";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+dotenv.config({path: './env'})  //require('dotenv').config({path: './env'})
+
+connectDB()
+
+
+
+
+
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       //(error) for recieving the error
+//       console.log("ERR:", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is Listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR:", error);
+//     throw err;
+//   }
+// })();

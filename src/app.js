81f2de520e43e6dcd5e76bf0b3026372536c49de
Kamna chauhan <kamnac78@gmail.com>
2025-12-30 20:03:57 +0530

    all setip configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,13 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+///ROUTES IMPORT
+
+import userRouter from "./routes/user.routes.js";
+
+//ROUTES DECLARATION
+
+
+app.use("/api/v1/users",userRouter)
+
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
- import express from "express";
- const app=express();
+ import {app} from "./app.js"
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 dotenv.config({path: './env'})  //require('dotenv').config({path: './env'})
